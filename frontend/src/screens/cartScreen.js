75cb2a7e8dd15c9aba3620e61d29100b19aa4ae6
Nzoputachi <samuelnzoputachi@gmail.com>
2024-07-24 14:19:47 +0100

    Refactor code

--- frontend/src/screens/cartScreen.js
+++ frontend/src/screens/cartScreen.js
@@ -2,7 +2,7 @@ import { getProduct } from "../api.js";
 import { getCartItems, setCartItems } from "../localStorage.js";
 import { parseRequestUrl, rerender } from "../utils.js";
 
-//add tocart function
+// add to cart function
 const addToCart = (item, forceUpdate = false) => {
   let cartItems = getCartItems();
   const existItem = cartItems.find(
@@ -18,90 +18,94 @@ const addToCart = (item, forceUpdate = false) => {
   }
 
   setCartItems(cartItems);
+  if (forceUpdate) {
+    rerender(CartScreen);
+  }
 };
 
 const removeFromCart = (id) => {
-    setCartItems(getCartItems().filter((x) => x.product !== id));
-    if (id === parseRequestUrl().id) {
-        document.location.hash = '/cart';
-    } else {
-        rerender(CartScreen);
-    }
+  setCartItems(getCartItems().filter((x) => x.product !== id));
+  if (id === parseRequestUrl().id) {
+    document.location.hash = '/cart';
+  } else {
+    rerender(CartScreen);
+  }
 };
 
 const CartScreen = {
   after_render: () => {
     document.querySelectorAll('.qty-select').forEach(qtySelect => {
-        qtySelect.addEventListener('change', (e) => {
-            const item = getCartItems().find((x) => x.product === qtySelect.id);
-            addToCart({...item, qty: Number(e.target.value)}, true)
-            rerender(CartScreen);
-        })
+      qtySelect.addEventListener('change', (e) => {
+        const item = getCartItems().find((x) => x.product === qtySelect.id);
+        addToCart({ ...item, qty: Number(e.target.value) }, true);
+      });
     });
 
-    //add event listener to the remove buttons
+    // add event listener to the remove buttons
     document.querySelectorAll('.remove-btn').forEach(removeButton => {
-        removeButton.addEventListener('click', () => {
-            removeFromCart(removeButton.id)
-        })
-    })
+      removeButton.addEventListener('click', () => {
+        removeFromCart(removeButton.id);
+      });
+    });
   },
 
-
   render: async () => {
-    //use parseRequestUrl to access the request parameters
+    // use parseRequestUrl to access the request parameters
     const request = parseRequestUrl();
 
-    //get product from the backend
+    // get product from the backend
     if (request.id) {
-      const product = await getProduct(request.id);
-      addToCart({
-        product: product._id,
-        name: product.name,
-        image: product.image,
-        price: product.price,
-        countInStock: product.countInStock,
-        qty: 1,
-      });
+      try {
+        const product = await getProduct(request.id);
+        addToCart({
+          product: product._id,
+          name: product.name,
+          image: product.image,
+          price: product.price,
+          countInStock: product.countInStock,
+          qty: 1,
+        });
+      } catch (error) {
+        console.log('Error fetching product:', error);
+      }
     }
 
-    
     const cartItems = getCartItems();
 
     return `
-    <section id="page-header"></section>
-    <section id="cart" class="section-p1">
+      <section id="page-header"></section>
+      <section id="cart" class="section-p1">
         <table width="100%">
-            <thead>
-                <tr>
-                    <td>Remove</td>
-                    <td>Images</td>
-                    <td>Product</td>
-                    <td>Price</td>
-                    <td>Quantity</td>
-                    <td>Subtotal</td>
-                </tr>
-            </thead>
-            <tbody>
-                ${
-                    cartItems.length === 0
-                    ? `<tr><td>Cart is empty.</td></tr>`
-                    : cartItems.map(item => `
-                    <tr>
-                        <td><button class="remove-btn" id="${item.product}"><i class="fa-regular fa-circle-xmark"></i></td>
-                        <td><img src="${item.image}" alt="${item.name}"></td>
-                        <td>${item.name}</td>
-                        <td>$${item.price}</td>
-                        <td>
-                            <input type="number" class="qty-select" id="${item.product}" value="${item.qty}" min="1" max="${item.countInStock}">
-                        </td>
-                        <td>$${item.price * item.qty}</td>
-                    </tr>
-                    `).join('\n')
-                }
-            </tbody>
+          <thead>
+            <tr>
+              <td>Remove</td>
+              <td>Images</td>
+              <td>Product</td>
+              <td>Price</td>
+              <td>Quantity</td>
+              <td>Subtotal</td>
+            </tr>
+          </thead>
+          <tbody>
+            ${
+              cartItems.length === 0
+                ? `<tr><td colspan="6">Cart is empty.</td></tr>`
+                : cartItems.map(item => `
+                  <tr>
+                    <td><button class="remove-btn" id="${item.product}"><i class="fa-regular fa-circle-xmark"></i></button></td>
+                    <td><img src="${item.image}" alt="${item.name}"></td>
+                    <td>${item.name}</td>
+                    <td>$${item.price}</td>
+                    <td>
+                      <input type="number" class="qty-select" id="${item.product}" value="${item.qty}" min="1" max="${item.countInStock}">
+                    </td>
+                    <td>$${item.price * item.qty}</td>
+                  </tr>
+                `).join('\n')
+            }
+          </tbody>
         </table>
-    </section>
+      </section>
     `;
   },
 };
