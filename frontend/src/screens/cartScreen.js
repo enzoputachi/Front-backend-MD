a64faa21374469e4c0a7f714d830ac283ede7362
Nzoputachi <samuelnzoputachi@gmail.com>
2024-07-28 19:15:22 +0100

    Update cart action UI

--- frontend/src/screens/cartScreen.js
+++ frontend/src/screens/cartScreen.js
@@ -76,43 +76,53 @@ const CartScreen = {
 
     return `
       <section id="page-header"></section>
-        <div class="cart-list">
-          <ul class="cart-list-container">
-            <li>
-              <h3>Shopping Cart</h3>
-              <div>Price</div>
-            </li>
-            ${
-              cartItems.length === 0?
-              '<div>Cart is empty. <a href="/#/">Go shopping</a>':
-              cartItems.map(item => `
-                <li>
-                  <div class="cart-image">
-                    <img src="${item.image}" alt="${item.name}" />
+      <div class="cart-list">
+        <ul class="cart-list-container">
+          <li>
+            <h3>Shopping Cart</h3>
+            <div>Price</div>
+          </li>
+          ${
+            cartItems.length === 0?
+            '<div>Cart is empty. <a href="/#/">Go shopping</a>':
+            cartItems.map(item => `
+              <li>
+                <div class="cart-image">
+                  <img src="${item.image}" alt="${item.name}" />
+                </div>
+                <div class="cart-name">
+                  <div>
+                    <a href="/#/product/${item.product}">
+                      ${item.name}
+                    </a>
                   </div>
-                  <div class="cart-name">
-                    <div>
-                      <a href="/#/product/${item.product}">
-                        ${item.name}
-                      </a>
-                    </div>
-                    <div>
-                      Qty: <select class="qty-select" id="${item.product}">
-                        <option value="1">1</option>
-                      </select>
-                      <button type="button" class="detete=button" id="${item.product}">
-                        Delete
-                      </button>
-                    </div>
+                  <div>
+                    Qty: <select class="qty-select" id="${item.product}">
+                      <option value="1">1</option>
+                    </select>
+                    <button type="button" class="detete=button" id="${item.product}">
+                      Delete
+                    </button>
                   </div>
-                  <div class="cart-price">
-                    $${item.price}
-                  </div>
-                </li>
-              `).join('')
-            }
-          </ul>
-        </div>
+                </div>
+                <div class="cart-price">
+                  $${item.price}
+                </div>
+              </li>
+            `).join('')
+          }
+        </ul>
+      </div>
+      <div class="cart-action">
+        <h3>
+          Subtotal (${cartItems.reduce((a, c) => a +c.qty, 0)} item)
+          : 
+          $${cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+        </h3>
+        <button id="checkout-button" class="primary fw">
+          Proceed to Checkout
+        </button>
+      </div>
     `;
   },
 };
