30148e5c96c2877dfea11ecca620897b3a9d5cdc
Nzoputachi <samuelnzoputachi@gmail.com>
2024-07-29 03:43:38 +0100

    Refactor code

--- frontend/script.js
+++ frontend/script.js
@@ -14,20 +14,3 @@ if (close) {
         nav.classList.remove('active')
     })
 } 
-
-//update the current page to the single product page
-document.addEventListener("DOMContentLoaded", function () {
-    const products = document.querySelectorAll('.pro');
-    if(products.length > 0) {
-        products.forEach(product => {
-            product.addEventListener('click', () => {
-                const productId = product.getAttribute('data-id');
-                if (productId) {
-                    window.location.href = `sproduct.html?id=${productId}`;
-                } else {
-                    console.log('product ID not found');
-                }
-            })
-        })
-    }
-})
