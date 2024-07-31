9879ecd662c238047921df27ca731e02e0726e2a
Nzoputachi <samuelnzoputachi@gmail.com>
2024-07-31 13:51:18 +0100

    Implement Signin page UI

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -4,19 +4,20 @@ const SigninScreen = {
         return `
         <div class="form-container">
             <form action="" id="signin-form">
+            <ul class="form-items">
                 <li>
                     <h1>Sign-In</h1>
                 </li>
                 <li>
                     <label for="email">Email</label>
-                    <input type="email" name="email" id="email" />
+                    <input class="email-input" type="email" name="email" id="email" />
                 </li>
                 <li>
                     <label for="passord">Password</label>
-                    <input type="password" name="password" id="password" />
+                    <input class="password-input" type="password" name="password" id="password" />
                 </li>
                 <li>
-                    <button type="submit" id="normal">Signin</button>
+                    <button type="submit" class="white" id="signin-button">Signin</button>
                 </li>
                 <li>
                     <div>
@@ -24,6 +25,7 @@ const SigninScreen = {
                         <a href="/#/register">Create your account</a>
                     </div>
                 </li>
+            </ul>
             </form>
         </div>
         `
