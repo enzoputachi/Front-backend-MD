85f45fc690e848c24993b42cadf1e348aa35cb69
Nzoputachi <samuelnzoputachi@gmail.com>
2024-08-02 16:40:05 +0100

    feat: add showMessage function to display overlay messages with callback support

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -26,3 +26,21 @@ export const showLoading = () => {
 export const hideLoading = () => {
     document.getElementById('loading-overlay').classList.remove('active');
 };
+
+//Show meaage overlay
+export const showMessage = (message, callback) => {
+    document.getElementById('message-overlay').innerHTML = `
+        <div>
+            <div id="message-overlay-content">${message}</div>
+            <button id="message-overlay-close-button" class="normal">OK</button>
+        </div>
+    `;
+    document.getElementById('message-overlay').classList.add('active')
+    document.getElementById('message-overlay-close-button')
+    .addEventListener('click', () => {
+        document.getElementById('message-overlay').classList.remove('active')
+        if(callback) {
+            callback();
+        }
+    })
+}
