b1a81273d138aa0aaf2a68bce02aac22c8df9da8
Nzoputachi <samuelnzoputachi@gmail.com>
2024-08-02 15:53:47 +0100

    Create showLoading and hideLoding functions

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -15,4 +15,14 @@ export const rerender = async (component) => {
     document.getElementById('main').innerHTML = await component.render();
     await component.after_render();
   };
-  
+
+
+// Show loading
+export const showLoading = () => {
+    document.getElementById('loading-overlay').classList.add('active');
+};
+
+// Hide loading
+export const hideLoading = () => {
+    document.getElementById('loading-overlay').classList.remove('active');
+};
