0f999e99e4918785f15e4e7d3ede56506e302a74
Nzoputachi <samuelnzoputachi@gmail.com>
2024-08-02 15:19:39 +0100

    Create a Header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,27 @@
+const Header = {
+    render: () => {
+        return `
+        <section id="header">
+            <a href="/"><img src="img/icon.png" class="logo" alt=""></a>
+            <div>
+                <ul id="navbar">
+                <li><a class="active" href="/">Home</a></li>
+                <!-- <li><a href="shop.html">shop</a></li> -->
+                <li><a href="/#/signin">Sign In</a></li>
+                <!-- <li><a href="#">About</a></li> -->
+                <!-- <li><a href="#">Contact</a></li> -->
+                <li><a id="lg-bag" href="/#/cart"><i class="fa-solid fa-shopping-bag"></i></a></li>
+                <a href="#" id="close"><i class="fas fa-times"></i></a>
+                </ul>
+            </div>
+            <div id="mobile">
+                <a href="/#/cart"><i class="fa-solid fa-shopping-bag"></i></a>
+                <i id="bar" class="fa-solid fa-bars"></i>
+            </div>
+        </section>
+        `
+    },
+    after_render: () => {},
+};
+
+export default Header;
