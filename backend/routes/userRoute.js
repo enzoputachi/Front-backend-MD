68205541871d3c42af6f6b888037956efdbfe47e
Nzoputachi <samuelnzoputachi@gmail.com>
2024-08-03 16:15:10 +0100

    Add a endpoint to handle user registration

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -28,6 +28,7 @@ userRouter.post("/signin", expressAsyncHandler(async (req, res) => {
     email: req.body.email,
     password: req.body.password
   });
+
   if(!signInUser) {
     res.status(401).send({
       message: 'Invalid email or password',
@@ -42,5 +43,32 @@ userRouter.post("/signin", expressAsyncHandler(async (req, res) => {
     })
   }
 }));
+
+
+userRouter.post("/register", expressAsyncHandler(async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  const createdUser = await user.save();
+
+  if(!createdUser) {
+    res.status(401).send({
+      message: 'Invalid user data',
+    })
+  } else {
+    res.send({
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      token: generateToken(createdUser),
+    })
+  }
+}));
+
+
 
 export default userRouter;
