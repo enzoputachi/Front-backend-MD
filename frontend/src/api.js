6f815fe900332933977c2f76ac7c61ee65abf196
Nzoputachi <samuelnzoputachi@gmail.com>
2024-08-04 13:37:01 +0100

    feat: add update function to update user profile details

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,6 @@
 import { apiUrl } from "./config.js";
 import axios from 'axios';
+import { getUserInfo } from "./localStorage.js";
 
 export const getProduct = async (id) => {
     try {
@@ -62,6 +63,34 @@ export const register = async ({ name, email, password }) => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+            },
+            data: { name, email, password, },
+        });
+
+        if(response.statusText !== 'OK') {
+            throw new Error(response.data.message);
+        }
+
+        return response.data;
+
+    } catch (error) {
+        console.log(error);
+        return { error: error.response?.data?.message || error.message }
+    }
+}
+
+export const update = async ({ name, email, password }) => {
+    
+    //Send HTTP request using Axios
+    try {
+        const {_id, token} = getUserInfo();
+
+        const response = await axios({
+            url: `${apiUrl}/api/users/${_id}`,
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
             },
             data: { name, email, password, },
         });
