d1d7de365d78de78e5946180af49f2272033c3ce
Nzoputachi <samuelnzoputachi@gmail.com>
2024-08-05 02:08:08 +0100

    feat: Add isAuth middleware to handle JWT authentication

--- backend/utils.js
+++ backend/utils.js
@@ -14,3 +14,20 @@ export const generateToken = (user) => {
 
     )
 }
+
+export const isAuth = (req, res, next) => {
+    const bearerToken = req.headers.authorization;
+    if (!bearerToken) {
+        res.status(401).send({ message: 'Token is not supplied' })
+    } else {
+        const token = bearerToken.slice(7, bearerToken.length);
+        jwt.verify(token, config.JWT_SECRET, (error, data) => {
+            if(error) {
+                res.status(401).send({ message: 'Invalid Token' });
+            } else {
+                req.user = data;
+                next();
+            }
+        });
+    }
+}  
