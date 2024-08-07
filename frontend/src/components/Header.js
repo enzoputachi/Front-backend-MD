ae145f827a963d2effd232c8ba368e6a9711756e
Nzoputachi <samuelnzoputachi@gmail.com>
2024-08-07 11:10:22 +0100

    fix bug

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = {
             <div>
                 <ul id="navbar">
                 <li><a class="active" href="/">Home</a></li>
-                <!-- <li><a href="shop.html">shop</a></li> -->
+                <li><a href="shop.html">shop</a></li>
                 <li>
                     ${
                         name
@@ -18,8 +18,8 @@ const Header = {
                         : `<a href="/#/signin">Sign In</a>`
                     }
                 </li>
-                <!-- <li><a href="#">About</a></li> -->
-                <!-- <li><a href="#">Contact</a></li> -->
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
                 <li><a id="lg-bag" href="/#/cart"><i class="fa-solid fa-shopping-bag"></i></a></li>
                 <a href="#" id="close"><i class="fas fa-times"></i></a>
                 </ul>
@@ -31,7 +31,22 @@ const Header = {
         </section>
         `
     },
-    after_render: () => {},
+    
+    after_render: () => {
+        const bar = document.getElementById('bar');
+        const nav = document.getElementById('navbar');
+        const close = document.getElementById('close');
+
+        if (bar) {
+            bar.addEventListener('click', () => {
+            nav.classList.add('active');
+        })} 
+
+        if (close) {
+            close.addEventListener('click', () => {
+            nav.classList.remove('active')
+        })}  
+    },
 };
 
 export default Header;
